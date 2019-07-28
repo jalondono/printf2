2e87c00500175e252e931997dae93e4c405ab60a
jalondono <juanlondono151776@hotmail.com>
2019-07-28 21:39:36 +0000

    upload format_string complete , checked

--- format_string.c
+++ format_string.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ *_memcpy - program thisgs
+ *Result: always return 0
+ *@dest: variable de entrada
+ *@src: variable entrada
+ *@n: variable entrada
+ *Return: value depending on function
+ */
+
+int *_memcpy(int *dest, int *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = src[i];
+	}
+	return (dest);
+}
+
+/**
+ *_strlen - program thisgs
+ *Result: always return 0
+ *@s: variable de entrada
+ *Return: value depending on function
+ */
 int _strlen(char *s)
 {
 	int c = 0;
@@ -9,31 +36,32 @@ int _strlen(char *s)
 	return (c);
 }
 
-int* format_string(char *s)
+/**
+ *format_string - encuentra la posicion de formatos de string
+ *Result: always return 0
+ *@s: string de entrada
+ *Return: un puntero con la posicion de cada formato encontrado
+ */
+int *format_string(char *s)
 {
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int c = 0;
-	int index = 0;
-	int exitflag = 0;
+	int i = 0, j = 0, k = 0, c = 0, exitflag = 0;
+	int *auxposformat, *posformat;
+	unsigned int index = 0;
 	char format[3] = "csd";
-	int *posformat;
 
 	if (s == NULL)
 		return (NULL);
-
-	posformat = malloc(sizeof(int) * _strlen(s));
-	if (posformat == NULL)
+	auxposformat = malloc(sizeof(int) * _strlen(s));
+	if (auxposformat == NULL)
 		return (NULL);
 	while (s[i])
 	{
 		exitflag = 0;
-		if(s[i] == '%')
+		if (s[i] == '%')
 		{
+			j = i + 1;
 			for (; s[j] != '\0'; j++)
 			{
-				j = i + 1;
 				if (s[j] == '%')
 				{
 					i = j + 1;
@@ -43,30 +71,17 @@ int* format_string(char *s)
 				{
 					if (s[j] == format[k])
 					{
-						posformat[index] = i;
-						index++;
-						exitflag = 1;
+						auxposformat[index] = i, index++, exitflag = 1;
 						break;
 					}
 				}
 				if (exitflag)
 					break;
 			}
-		}
-
-		i++;
-	}
-}
-
-int main()
-{
-	int *posformat;
-	posformat = format_string("ab %s t %d %c y%c");
-	if (posformat != NULL)
-	{
-		printf("%d, %d", posformat[0], posformat[1]);
-		free(posformat);
-	}
-
-	return 0;
+		} i++;
+	} posformat = malloc(sizeof(int) * index);
+	if (posformat == NULL)
+		return (NULL);
+	posformat = _memcpy(posformat, auxposformat, index), free(auxposformat);
+	return (posformat);
 }
