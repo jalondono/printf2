6200e58dabddd9aeabbf75ed9f9203aea6a2ed4a
jalondono <juanlondono151776@hotmail.com>
2019-07-30 21:18:56 +0000

    Revert "corrigiendo el printf de INT MIN"
    This reverts commit e46b1b27740bc170a79f17e895212da3b90a6f86.
    
    Love you

--- print_integer.c
+++ print_integer.c
@@ -8,27 +8,20 @@
 int print_integer_d(int number)
 {
 	int result = 0;
-	unsigned int aux = 0;
-	int flag = 0;
 
 	if (number < 0)
 	{
 		_putchar('-');
-		aux = -number;
-		flag = 1;
+		number = -number;
 	}
-	else
-		aux = number;
-	if (aux / 10)
-	{
-		print_integer_d(aux / 10);
-	}
-	_putchar(aux % 10 + '0');
+	if (number / 10)
+		print_integer_d(number / 10);
+	_putchar(number % 10 + '0');
 
-	while (aux / 10 > 0)
+	while (number / 10 > 0)
 	{
-		aux = aux / 10;
+		number = number / 10;
 		result++;
 	}
-	return (result + 1 + flag);
+	return (result + 1);
 }
